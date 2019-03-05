4bbc965423e4d3764688a765006dee0f4385fd02
bolivierjr <bolivierjr@gmail.com>
2019-03-05 09:05:19 -0600

    Adding error logging and a better way to close the browser

--- scrape.py
+++ scrape.py
@@ -1,41 +1,49 @@
 import sys
+import logging
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 
+
 def get_data(url):
     """
-    Uses selenium webdriver to return the page source html
+    Uses selenium webdriver to scrape html off a page
 
     Args:
-        (str): url
-    """
-    opts = Options()
-    opts.headless = True
-    browser = Firefox(options=opts)
+        url(str) - The url of the site to scrape.
 
+    Returns:
+        (str) - Returns the whole html source
+    """
     try:
         if not url.startswith('http'):
             raise Exception('Must be a valid url')
-    
-        browser.get(url)
-        source = browser.page_source
-    
-        return source
-    
+
+        opts = Options()
+        opts.headless = True
+
+        with Firefox(options=opts) as browser:
+            browser.get(url)
+            source = browser.page_source
+
+            return source
+
     except Exception as err:
+        logging.error(f'{err}', exc_info=True)
         print(err)
         sys.exit(1)
-    
-    finally:
-        browser.close()
+
 
 if __name__ == '__main__':
     import argparse
-    
+
     parser = argparse.ArgumentParser()
     required = parser.add_argument_group('required arguments')
     required.add_argument('-u', '--url', action='store', dest='url', required=True,
                           type=str, help='Enter in the url you want to scrape.')
     args = parser.parse_args()
+
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                        filename='error.log',
+                        datefmt='%d-%b-%y %H:%M:%S')
 
     print(get_data(args.url))
